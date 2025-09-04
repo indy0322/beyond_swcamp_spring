342fea27d8c36a14ee7885ff12150cc92ca294a0
indy032229@gmail.com <indy032229@gmail.com>
2025-09-04 11:49:58 +0900

    2025/09/04

---
+++ chap01-bean-factory-lecture-source/src/main/java/com/ohgiraffers/common/MemberDAO.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberDAO {
+    public MemberDAO() {
+        System.out.println("콩 될 때 이거 쓰나?");
+    }
+}
---
+++ chap01-bean-factory-lecture-source/src/main/java/com/ohgiraffers/section03/annotaionconfig/subsection01/java/ContextConfiguration.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section03.annotaionconfig.subsection01.java;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ContextConfiguration {
+
+}
